bb75b8548c662f8404136c0d9647d58a2af8167c
unknown <kumarasitorus@gmail.com>
2020-11-18 17:54:06 +0700

    ADT point, mesinkata, mesinkar

---
+++ mesinkar.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "mesinkar.h"
+
+static FILE * pita;
+static int retval;
+
+void START(char* filename)
+/* Mesin siap dioperasikan. Pita disiapkan untuk dibaca.
+Karakter pertama yang ada pada pita posisinya adalah pada jendela.
+filename merupakan nama file yang berisi pita karakter
+I.S. : sembarang
+F.S. : CC adalah karakter pertama pada pita
+Jika CC != MARK maka EOP akan padam (false)
+Jika CC = MARK maka EOP akan menyala (true) */
+{
+    pita = fopen("pitakar.txt","r");
+    ADV();
+}
+
+void ADV()
+/* Pita dimajukan satu karakter.
+I.S. : Karakter pada jendela = CC, CC != MARK
+F.S. : CC adalah karakter berikutnya dari CC yang lama,
+
+CC mungkin = MARK
+Jika CC = MARK maka EOP akan menyala (true) */
+{
+    retval = fscanf(pita,"%c",&CC);
+    EOP = (CC == MARK);
+    if (EOP) {
+    fclose(pita);
+    }
+}
---
+++ mesinkar.h
@@ -0,0 +1,35 @@
+#ifndef __MESIN_KAR__
+#define __MESIN_KAR__
+
+#include "boolean.h"
+#include <stdio.h>
+#define MARK '.'
+
+/* State Mesin */
+extern char CC;
+extern boolean EOP;
+
+/*
+  pada implementasi (mesin_kar.c), perlu ditambahkan variabel static 
+  global yang menyimpan pembacaan file pita.
+*/
+
+void START(char* filename);
+/* Mesin siap dioperasikan. Pita disiapkan untuk dibaca.
+Karakter pertama yang ada pada pita posisinya adalah pada jendela.
+filename merupakan nama file yang berisi pita karakter
+I.S. : sembarang
+F.S. : CC adalah karakter pertama pada pita
+
+Jika CC != MARK maka EOP akan padam (false)
+Jika CC = MARK maka EOP akan menyala (true) */
+
+void ADV();
+/* Pita dimajukan satu karakter.
+I.S. : Karakter pada jendela = CC, CC != MARK
+F.S. : CC adalah karakter berikutnya dari CC yang lama,
+
+CC mungkin = MARK
+Jika CC = MARK maka EOP akan menyala (true) */
+
+#endif
